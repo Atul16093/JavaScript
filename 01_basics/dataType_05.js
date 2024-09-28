5c96b370e946e5a34ee84dc1d1f8b3e67534bf18
Atul16093 <161714875+Atul16093@users.noreply.github.com>
2024-09-28 04:20:33 +0000

    JavaScript DataType

---
+++ 01_basics/dataType_05.js
@@ -0,0 +1,22 @@
+let name = "JavaScript";
+let id = 12;
+let flag = true;
+let f = 12.5
+let standard;
+
+/*
+Data type in javaScript
+String => ""
+number => 21;
+boolean = True/false
+null => It's a stand alone data type 
+undefined => means we not define value yet .
+symbol => unique
+
+
+object 
+ */
+
+console.log(typeof name);
+console.log(typeof null)
+console.log(typeof f)
