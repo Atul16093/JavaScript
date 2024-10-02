5c550c7c612b43f480127e79002800cc89b47053
Atul <atulrag3@gmail.com>
2024-10-02 18:13:05 +0530

    Nums and Maths

--- Learning/Tutorial_12/Nums_and_Maths.js
+++ Learning/Tutorial_12/Nums_and_Maths.js
@@ -64,4 +64,6 @@ console.log(Math.random() *10 +1);
 const min = 10; 
 const max = 20;
 
-console.log((Math.random() * (max-min +1) + min));
+//Here is the little formula for get two digit value.
+
+console.log(Math.floor(Math.random() * (max-min +1) + min)); 
