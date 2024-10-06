5e2246783b7669a85309d070ab4c858a7a8f834a
Atul <atulrag3@gmail.com>
2024-10-06 17:36:47 +0530

    Common loops

---
+++ Learning/Tutorial_27/Loops.js
@@ -0,0 +1,5 @@
+/*The continue keyword in Java (and many other programming languages) is used to skip the current iteration
+ of a loop and move on to the next iteration. It's typically used within for, while,
+ or do-while loops when you want to skip certain steps based on a condition, without 
+exiting the loop entirely (which would be done using break). */
+
